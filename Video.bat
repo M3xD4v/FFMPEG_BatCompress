03b96774becea6b35f5eb14a2f1cc26059f59e3c
M3xD4v <65831130+M3xD4v@users.noreply.github.com>
2024-06-29 23:48:38 +0300

    Add files via upload

--- Video.bat
+++ Video.bat
@@ -1,11 +1,18 @@
 @echo off
 
+rem Generate a random 3-digit number
+set /a random_id=%random% %% 900 + 100
+echo Your random 3-digit ID is: %random_id%
+
+
 if %2 == "low" (
-    ffmpeg -hwaccel cuda -i %1 -c:v h264_nvenc -crf 28 -b:v 1000k -c:a aac -b:a 96k "low_Compressed.mp4"
+    ffmpeg -hwaccel cuda -i %1 -c:v h264_nvenc -crf 28 -b:v 1000k -c:a aac -b:a 96k "low_%random_id%.mp4"
 ) else if %2 == "high" (
-    ffmpeg -hwaccel cuda -i %1 -c:v h264_nvenc -crf 25 -b:v 2000k -c:a aac -b:a 128k "high_Compressed.mp4"
+    ffmpeg -hwaccel cuda -i %1 -c:v h264_nvenc -crf 25 -b:v 2000k -c:a aac -b:a 128k "high_%random_id%.mp4"
 ) else if %2 == "ultra" (
-    ffmpeg -hwaccel cuda -i %1 -c:v h264_nvenc -crf 18 -b:v 4000k -c:a aac -b:a 192k "ultra_Compressed.mp4"
+    ffmpeg -hwaccel cuda -i %1 -c:v h264_nvenc -crf 18 -b:v 4000k -c:a aac -b:a 192k "ultra_%random_id%.mp4"
 ) else if %2 == "mute" (
     ffmpeg -i %1 -c: copy -an "Muted.mp4"
+) else if %2 == "speed" (
+    ffmpeg -hwaccel cuda -i %1 -vf "setpts=PTS/2" -an "speed_%random_id%.mp4"
 )
